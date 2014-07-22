283ce57651f733801c0a449fedde813bd0b9d442
nickbarth <nickbarth@live.com>
2014-07-22 11:37:51 -0600

    added link account

--- src/ios/iOSTwitterFriends.m
+++ src/ios/iOSTwitterFriends.m
@@ -7,6 +7,18 @@
 - (void)iOSTwitterFriends:(CDVInvokedUrlCommand *)command {
     self.callbackId = command.callbackId;
 
+    PFUser *currentUser = [PFUser currentUser];
+    if (![PFTwitterUtils isLinkedWithUser:currentUser]) {
+        [PFTwitterUtils linkUser:currentUser block:^(BOOL succeeded, NSError *error) {
+            if (error) {
+                NSString *result = [NSString stringWithFormat:@"{ \"error\": \"%@\" }", error];
+                CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:result];
+                NSString* javaScript = [pluginResult toSuccessCallbackString:self.callbackId];
+                [self writeJavascript:javaScript];
+            }
+        }];
+    }
+
     NSURL *url = [NSURL URLWithString:@"https://api.twitter.com/1.1/friends/list.json"];
     NSOperationQueue *queue = [[NSOperationQueue alloc] init];
     NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:url];
